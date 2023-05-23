f7fb715235a9557b95c1abf51759a244ddb23de2
jablob44 <jacob@ifelse.ai>
2023-05-23 12:59:08 -0700

    told the model selection to fuck off

--- pages/api/getEngines.ts
+++ pages/api/getEngines.ts
@@ -1,27 +1,28 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import openai from "@/utils/chatgpt";
+export{}
+// import { NextApiRequest, NextApiResponse } from "next";
+// import openai from "@/utils/chatgpt";
 
-type Option = {
-  value: string;
-  label: string;
-};
+// type Option = {
+//   value: string;
+//   label: string;
+// };
 
-type Data = {
-  modelOption: Option[];
-};
+// type Data = {
+//   modelOption: Option[];
+// };
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  const models = await openai.listModels().then((res) => res.data.data);
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse<Data>
+// ) {
+//   const models = await openai.listModels().then((res) => res.data.data);
 
-  const modelOption = models.map((model) => ({
-    value: model.id,
-    label: model.id,
-  }));
+//   const modelOption = models.map((model) => ({
+//     value: model.id,
+//     label: model.id,
+//   }));
 
-  res.status(200).json({
-    modelOption,
-  });
-}
+//   res.status(200).json({
+//     modelOption,
+//   });
+// }
